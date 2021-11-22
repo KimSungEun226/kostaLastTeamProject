0eb03c82883108b06f1fa0ea25dae3d5ca763cf1
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-11-22 23:08:24 +0900

    Initial setting

---
+++ src/main/java/kosta/mvc/controller/IndexController.java
@@ -0,0 +1,13 @@
+package kosta.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	 public String index() {
+		 return "index";// /WEB-INF/views/index.jsp이동
+	 }
+}
