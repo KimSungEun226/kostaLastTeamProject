eadf202ab100578a1934209f1e888f9ba0a91f7a
isshoeunji <88135219+isshoeunji@users.noreply.github.com>
2021-11-29 22:15:03 +0900

    자유게시판 프론트 연결

--- src/main/java/kosta/mvc/service/BoardServiceImpl.java
+++ src/main/java/kosta/mvc/service/BoardServiceImpl.java
@@ -3,13 +3,23 @@ package kosta.mvc.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import kosta.mvc.domain.Board;
+import kosta.mvc.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardServiceImpl implements BoardService {
 
+	private final BoardRepository boardRepository;
+	
 	@Override
 	public List<Board> selectAll() {
 		// TODO Auto-generated method stub
@@ -18,8 +28,7 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public Page<Board> selectAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+		return boardRepository.findAll(pageable);
 	}
 
 	@Override
