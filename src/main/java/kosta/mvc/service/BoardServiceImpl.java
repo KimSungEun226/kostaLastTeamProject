317aa9cab48dde2cea585fd92476f18978ef3316
isshoeunji <88135219+isshoeunji@users.noreply.github.com>
2021-11-29 11:41:00 +0900

    Board 테스트, BoardService 추가

---
+++ src/main/java/kosta/mvc/service/BoardServiceImpl.java
@@ -0,0 +1,48 @@
+package kosta.mvc.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import kosta.mvc.domain.Board;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public List<Board> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Board> selectAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Board board) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Board selectBy(Long boardNo, boolean state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Board update(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long boardNo, String password) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
