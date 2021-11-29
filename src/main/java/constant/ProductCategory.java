afd57683ad60ae819b1c3673511ae09fbac1e0db
KimSungEun226 <57550856+KimSungEun226@users.noreply.github.com>
2021-11-29 18:10:53 +0900

    상품 목록 보여주기

--- src/main/java/constant/ProductCategory.java
+++ src/main/java/constant/ProductCategory.java
@@ -2,7 +2,7 @@ package constant;
 
 public interface ProductCategory {
 
-	public static final int PROTEIN_NO = 1;
-	public static final int SALAD_NO = 1;
+	public static final int PROTEIN_NO = 3;
+	public static final int SALAD_NO = 2;
 	public static final int CHICKEN_BREAST_NO = 1;
 }
--- src/main/java/kosta/mvc/service/ProductService.java
+++ src/main/java/kosta/mvc/service/ProductService.java
@@ -1,5 +1,7 @@
 package kosta.mvc.service;
 
+import java.util.List;
+
 import kosta.mvc.domain.product.Product;
 
 public interface ProductService {
@@ -9,4 +11,9 @@ public interface ProductService {
 	 * */
 	void insert(Product product);
 	
+	
+	/**
+	 * 카테고리별 검색
+	 * */
+	List<Product> selectByCateCode(int cateCode);
 }
