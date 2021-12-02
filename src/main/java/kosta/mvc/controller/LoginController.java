2bd909f999262d86b131b8a8beabbc7ec5b5216c
KimSungEun226 <57550856+KimSungEun226@users.noreply.github.com>
2021-12-03 00:26:57 +0900

    멀티시큐리티적용수정

--- src/main/java/kosta/mvc/controller/LoginController.java
+++ src/main/java/kosta/mvc/controller/LoginController.java
@@ -19,6 +19,12 @@ public class LoginController {
     	return "board/signup";
     }
     
+
+    // 회원가입 페이지
+    @GetMapping("shop/signup")
+    public String shopSignup() {
+    	return "shop/signup";
+    }
     
 
     // 회원가입 처리
@@ -28,11 +34,18 @@ public class LoginController {
 
         return "redirect:/login";
     }
+    
+    // 쇼핑몰 회원가입 처리
+    @PostMapping("shop/signup")
+    public String shopSignup(MemberDto memberDto) {
+        memberService.joinUser(memberDto);
+
+        return "redirect:/shop/login";
+    }
 
     // 로그인 페이지
     @GetMapping("/login")
     public String dispLogin() {
-    	System.out.println("LgingControllerdㅡ ㅣㅐ햐ㅜ...................");
         return "board/login";
     }
     
@@ -42,6 +55,13 @@ public class LoginController {
         return "shop/login";
     }
 
+   // 쇼핑몰 로그인 결과 페이지
+    @GetMapping("/shop/main")
+    public String shopLoginResult() {
+    	System.out.println("로그인성공");
+        return "redirect:/shop";
+    }
+    
     // 로그인 결과 페이지
     @GetMapping("/user/main")
     public String dispLoginResult() {
@@ -57,7 +77,7 @@ public class LoginController {
     // 접근 거부 페이지
     @GetMapping("/user/denied")
     public String dispDenied() {
-        return "/denied";
+        return "/error/errorView";
     }
 
     // 내 정보 페이지
